bd9a76681f4776f498eb09b363f654bf996233bf
James <james.kukreja@twinspark.co>
2016-08-23 18:19:14 +0530

    get associations

--- src/Controller/BookmarksController.php
+++ src/Controller/BookmarksController.php
@@ -29,7 +29,11 @@ class BookmarksController extends AppController
 
     public function export($limit=100){
 
-        $bookmarks = $this->Bookmarks->find('all')->limit($limit);
+        $bookmarks = $this->Bookmarks->find('all')->limit($limit)
+        ->where(['user_id' => 1])
+        ->contain(['Tags'=> function($q){
+            return $q->where(['Tags.title Like' => '%n%']);
+        }]);
         $this->set('bookmarks',$bookmarks);
     }
 
